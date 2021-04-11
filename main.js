4dc3a816851166d947db822f8d48c79e6dc3199b
Marvellous <57817706+Marvvy-G@users.noreply.github.com>
2021-04-11 03:58:21 +0100

    done

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelector('.nav-links');
+    
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    })	
+    
+    }
